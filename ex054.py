47df407ee4a314ead334708e56a7870c6b64d13a
DiegoMMoura <104261990+DiegoMMoura@users.noreply.github.com>
2022-11-22 22:24:42 -0300

    Update ex054.py

--- ex054.py
+++ ex054.py
@@ -1,8 +1,12 @@
 from datetime import date
-ano = 0
-for c in range(0, 3):
-    ano = int(input(f'What year the {c} person was born? '))
-today = date.now()
-print(today)
-#   if ano <= (today + 18):
-#        print()
+year_low = 0
+year_high = 0
+for c in range(1, 4 ):
+    ano = int(input(f'What year the {c}º person was born? '))
+    today = date.today().year
+    if today - ano < 21:
+        year_low += 1
+    else:
+        year_high += 1
+print(f'There are {year_low} person(s) under age.')
+print(f'There are {year_high} person(s) of legal age')
